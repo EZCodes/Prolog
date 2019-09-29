6f56cc6c835caab68abb6a444b468843bba636d0
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-09-29 21:25:12 +0100

    finished exercise2

--- assignment1.pl
+++ assignment1.pl
@@ -9,13 +9,16 @@ add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 
 add2(0,0,0).
+add2(0,s(p(X)),Z) :- add2(0,X,Z).
+add2(0,p(s(X)),Z) :- add2(0,X,Z).
 add2(0,s(X),s(Z)) :- add2(0,X,Z).
 add2(0,p(X),p(Z)) :- add(0,X,Z).
 add2(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), add2(S,W,Z).
 add2(X+Y,Q,Z) :- add2(X,Y,S), add2(S,Q,Z).
 add2(X,Y+Q,Z) :- add2(Y,Q,S), add2(X,S,Z).
 
-
+add2(p(0),Y,Z) :- add2(0,p(Y),Z).
+add2(s(0),Y,Z) :- add2(0,s(Y),Z).
 add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
 add2(s(p(X)),Y,Z) :- add2(X,Y,Z).
 add2(p(X),Y,p(Z)) :- add2(X,Y,Z).
