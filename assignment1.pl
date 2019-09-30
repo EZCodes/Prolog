fd52afd95656fe3303019549c608a7d6b5f757f4
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-09-30 17:56:32 +0100

    Finished all exercises

--- assignment1.pl
+++ assignment1.pl
@@ -3,6 +3,7 @@ numeral(s(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 numeral(p(X)) :- numeral(X).
 numeral(-X) :- numeral(X).
+numeral(X-Y) :- numeral(X), numeral(Y).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
@@ -19,6 +20,10 @@ add2((-X),(-Y),Z) :- minus(X,S), minus(Y,Q), add2(S,Q,Z).
 add2((-X),Y,Z) :- minus(X,S), add2(S,Y,Z).
 add2(X,(-Y),Z) :- minus(Y,S), add2(X,S,Z).
 
+add2(X-Y,Q-V,Z) :- subtract(X,Y,S), subtract(Q,V,W), add2(S,W,Z).
+add2(X-Y,Q,Z) :- subtract(X,Y,S), add2(S,Q,Z).
+add2(X,Y-Q,Z) :- subtract(Y,Q,S), add2(X,S,Z).
+
 add2(p(0),Y,Z) :- add2(0,p(Y),Z).
 add2(s(0),Y,Z) :- add2(0,s(Y),Z).
 add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
@@ -31,9 +36,19 @@ minus(p(s(X)),Z) :- minus(X,Z).
 minus(s(p(X)),Z) :- minus(X,Z).
 minus(p(X),s(Z)) :- minus(X,Z).
 minus(s(X),p(Z)) :- minus(X,Z).
+minus(X-Y,Z) :- subtract(X,Y,S), minus(S,Z).
+minus(X+Y,Z) :- add2(X,Y,S), minus(S,Z).
 
 subtract(X,Y,Z) :- minus(Y,S), add2(X,S,Z).
 subtract(X,(-Y),Z) :- add2(X,Y,Z).
+
+subtract(X-Y,Q-V,Z) :- subtract(X,Y,S), subtract(Q,V,W), subtract(S,W,Z).
+subtract(X-Y,Q,Z) :- subtract(X,Y,S), subtract(S,Q,Z).
+subtract(X,Y-Q,Z) :- subtract(Y,Q,S), subtract(X,S,Z).
+subtract(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), subtract(S,W,Z).
+subtract(X+Y,Q,Z) :- add2(X,Y,S), subtract(S,Q,Z).
+subtract(X,Y+Q,Z) :- add2(Y,Q,S), subtract(X,S,Z).
+
 
 
 
