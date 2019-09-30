6bae5fe185606cecbc7a12b39c2a2dcc4741d328
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-09-30 10:46:13 +0100

    finished exercise 4

--- assignment1.pl
+++ assignment1.pl
@@ -2,8 +2,7 @@ numeral(0).
 numeral(s(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 numeral(p(X)) :- numeral(X).
-
-
+numeral(-X) :- numeral(X).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
@@ -16,6 +15,9 @@ add2(0,p(X),p(Z)) :- add(0,X,Z).
 add2(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), add2(S,W,Z).
 add2(X+Y,Q,Z) :- add2(X,Y,S), add2(S,Q,Z).
 add2(X,Y+Q,Z) :- add2(Y,Q,S), add2(X,S,Z).
+add2((-X),(-Y),Z) :- minus(X,S), minus(Y,Q), add2(S,Q,Z).
+add2((-X),Y,Z) :- minus(X,S), add2(S,Y,Z).
+add2(X,(-Y),Z) :- minus(Y,S), add2(X,S,Z).
 
 add2(p(0),Y,Z) :- add2(0,p(Y),Z).
 add2(s(0),Y,Z) :- add2(0,s(Y),Z).
@@ -29,4 +31,6 @@ minus(p(s(X)),Z) :- minus(X,Z).
 minus(s(p(X)),Z) :- minus(X,Z).
 minus(p(X),s(Z)) :- minus(X,Z).
 minus(s(X),p(Z)) :- minus(X,Z).
+
+
 
