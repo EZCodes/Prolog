3a3e03f6124ab1b5978998ac10d453d3b4cf73b4
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-10-14 17:04:13 +0100

    no longer gives wrong answers on

--- assignment1.pl
+++ assignment1.pl
@@ -11,14 +11,23 @@ add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add2(0,0,0).
 add2(0,s(p(X)),Z) :- add2(0,X,Z).
 add2(0,p(s(X)),Z) :- add2(0,X,Z).
-add2(0,s(X),s(Z)) :- add2(0,X,Z).
-add2(0,p(X),p(Z)) :- add2(0,X,Z).
+add2(0,s(s(X)),s(Z)) :- add2(0,s(X),Z).
+add2(0,p(p(X)),p(Z)) :- add2(0,p(X),Z).
+add2(0,p(X+Y),p(Z)) :- add2(0,X,S), add2(0,Y,Q), add2(S,Q,Z). 
+add2(0,s(X+Y),s(Z)) :- add2(0,X,S), add2(0,Y,Q), add2(S,Q,Z). 
+add2(0,p(X-Y),p(Z)) :- add2(0,X,S), add2(0,Y,Q), subtract(S,Q,Z). 
+add2(0,s(X-Y),s(Z)) :- add2(0,X,S), add2(0,Y,Q), subtract(S,Q,Z). 
+add2(0,s(-X),Z) :- minus(X,S), add2(0,s(S),Z).
+add2(0,p(-X),Z) :- minus(X,S), add2(0,p(S),Z).
+
+add2(0,s(0),s(0)).
+add2(0,p(0),p(0)).
 
 add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
 add2(s(p(X)),Y,Z) :- add2(X,Y,Z).
 
 add2(s(X),p(Y),Z) :- add2(X,Y,Z).
-add2(p(X),x(Y),Z) :- add2(X,Y,Z).
+add2(p(X),s(Y),Z) :- add2(X,Y,Z).
 add2(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), add2(S,W,Z).
 add2(X+Y,Q,Z) :- add2(X,Y,S), add2(S,Q,Z).
 add2(X,Y+Q,Z) :- add2(Y,Q,S), add2(X,S,Z).
@@ -33,14 +42,16 @@ add2(X,Y-Q,Z) :- subtract(Y,Q,S), add2(X,S,Z).
 add2(p(0),Y,Z) :- add2(0,p(Y),Z).
 add2(s(0),Y,Z) :- add2(0,s(Y),Z).
 
-add2(p(X),Y,p(Z)) :- add2(X,Y,Z).
-add2(s(X),Y,s(Z)) :- add2(X,Y,Z).
+add2(p(X),p(Y),p(Z)) :- add2(X,p(Y),Z).
+add2(s(X),s(Y),s(Z)) :- add2(X,s(Y),Z).
 
 minus(0,0).
+minus(s(0),p(0)).
+minus(p(0),s(0)).
 minus(p(s(X)),Z) :- minus(X,Z).
 minus(s(p(X)),Z) :- minus(X,Z).
-minus(p(X),s(Z)) :- minus(X,Z).
-minus(s(X),p(Z)) :- minus(X,Z).
+minus(p(p(X)),s(Z)) :- minus(p(X),Z).
+minus(s(s(X)),p(Z)) :- minus(s(X),Z).
 minus(X-Y,Z) :- subtract(X,Y,S), minus(S,Z).
 minus(X+Y,Z) :- add2(X,Y,S), minus(S,Z).
 minus((-X),Z) :- minus(X,S), minus(S,Z).
