571840f3570895edb8770ee080a85a0cc3685258
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-10-14 11:50:59 +0100

    fixed small bug

--- assignment1.pl
+++ assignment1.pl
@@ -12,7 +12,7 @@ add2(0,0,0).
 add2(0,s(p(X)),Z) :- add2(0,X,Z).
 add2(0,p(s(X)),Z) :- add2(0,X,Z).
 add2(0,s(X),s(Z)) :- add2(0,X,Z).
-add2(0,p(X),p(Z)) :- add(0,X,Z).
+add2(0,p(X),p(Z)) :- add2(0,X,Z).
 add2(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), add2(S,W,Z).
 add2(X+Y,Q,Z) :- add2(X,Y,S), add2(S,Q,Z).
 add2(X,Y+Q,Z) :- add2(Y,Q,S), add2(X,S,Z).
