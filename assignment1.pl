f8cfcfcabdd3fc185e4655fab89d400340d92f41
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-10-14 13:19:09 +0100

    small bug fixes

--- assignment1.pl
+++ assignment1.pl
@@ -13,6 +13,12 @@ add2(0,s(p(X)),Z) :- add2(0,X,Z).
 add2(0,p(s(X)),Z) :- add2(0,X,Z).
 add2(0,s(X),s(Z)) :- add2(0,X,Z).
 add2(0,p(X),p(Z)) :- add2(0,X,Z).
+
+add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
+add2(s(p(X)),Y,Z) :- add2(X,Y,Z).
+
+add2(s(X),p(Y),Z) :- add2(X,Y,Z).
+add2(p(X),x(Y),Z) :- add2(X,Y,Z).
 add2(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), add2(S,W,Z).
 add2(X+Y,Q,Z) :- add2(X,Y,S), add2(S,Q,Z).
 add2(X,Y+Q,Z) :- add2(Y,Q,S), add2(X,S,Z).
@@ -26,8 +32,7 @@ add2(X,Y-Q,Z) :- subtract(Y,Q,S), add2(X,S,Z).
 
 add2(p(0),Y,Z) :- add2(0,p(Y),Z).
 add2(s(0),Y,Z) :- add2(0,s(Y),Z).
-add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
-add2(s(p(X)),Y,Z) :- add2(X,Y,Z).
+
 add2(p(X),Y,p(Z)) :- add2(X,Y,Z).
 add2(s(X),Y,s(Z)) :- add2(X,Y,Z).
 
@@ -49,7 +54,6 @@ subtract(X,Y-Q,Z) :- subtract(Y,Q,S), subtract(X,S,Z).
 subtract(X+Y,Q+V,Z) :- add2(X,Y,S), add2(Q,V,W), subtract(S,W,Z).
 subtract(X+Y,Q,Z) :- add2(X,Y,S), subtract(S,Q,Z).
 subtract(X,Y+Q,Z) :- add2(Y,Q,S), subtract(X,S,Z).
-
 
 
 
