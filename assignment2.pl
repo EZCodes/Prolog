f62612ed779d3ff25fdb08efbb370e1d743288f6
Jerzy Jaskuc <jaskucj@tcd.ie>
2019-11-11 17:12:03 +0000

    ready for submission

--- assignment2.pl
+++ assignment2.pl
@@ -1,6 +1,6 @@
 %Exercise 1
 
-s --> ublock(Count), vblock(CountTwo),{ Count is CountTwo * 2 }.
+s1 --> ublock(Count), vblock(CountTwo),{ Count is CountTwo * 2 }.
 
 ublock(0) --> [2].
 ublock(NewCount) --> [0], ublock(Count), {NewCount is Count+1}.
@@ -12,7 +12,7 @@ vblock(NewCount) --> [1], vblock(Count), {NewCount is Count+1}.
 
 %Exercise 2
 
-s --> col(X),nat(Q),pet(A),col(Y),nat(W),pet(S),col(Z),nat(E),pet(D),{X=\=Y,Y=\=Z,X=\=Z, Q=\=W,W=\=E,Q=\=E, A=\=S,S=\=D,D=\=A}.
+s2 --> col(X),nat(Q),pet(A),col(Y),nat(W),pet(S),col(Z),nat(E),pet(D),{X=\=Y,Y=\=Z,X=\=Z, Q=\=W,W=\=E,Q=\=E, A=\=S,S=\=D,D=\=A}.
 col(1) --> [red].
 col(2) --> [green].
 col(3) --> [blue].
@@ -26,12 +26,12 @@ pet(2) --> [snail].
 pet(3) --> [zebra].
 
 %Exercise 3
-s(0) --> [].
+s3(0) --> [].
 
-s(A, B, D) :- mkList(A, C), sum(A, C, B, E),  D=E.
+s3(A, B, D) :- mkList(A, C), sum(A, C, B, E),  D=E.
 
 sum(Count,[H|_]) --> [H], {H is Count }.
-sum(C, [A|_], [A|B], E) :- A<C, D is C-A, s(D, B, F), E=F.
+sum(C, [A|_], [A|B], E) :- A<C, D is C-A, s3(D, B, F), E=F.
 sum(C, [_|A], B, E) :- sum(C, A, B, F), E=F.
 
 mkList(0,[]). 
